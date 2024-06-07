e63dbecc3bba12ba3b3ebd9a34739b9f47a6d0a6
Anastasia Begun <nbegun_314@mail.ru>
2024-06-07 13:29:04 +0300

    c1

---
+++ nsi_parsing.py
@@ -0,0 +1,185 @@
+import json
+import os
+import zipfile
+from pprint import pprint
+from typing import Generator, Dict, Any, List
+import requests
+import chardet
+import lxml.html
+
+
+class RequestHandler:
+    """
+    Класс для отправки HTTP-запросов. Поддерживает выборочное игнорирование SSL-сертификатов.
+    """
+
+    def __init__(self, verify_ssl: bool = False):
+        """
+        Инициализация RequestHandler.
+
+        :param verify_ssl: Проверять ли SSL-сертификаты.
+        """
+        self.verify_ssl = verify_ssl
+        if not verify_ssl:
+            requests.packages.urllib3.disable_warnings(
+                requests.packages.urllib3.exceptions.InsecureRequestWarning
+            )
+
+    def get(self, url: str, **kwargs) -> requests.Response:
+        """
+        Выполняет GET-запрос.
+
+        :param url: URL для GET-запроса.
+        :return: Объект ответа requests.
+        """
+        return requests.get(url, verify=self.verify_ssl, **kwargs)
+
+
+class NSIClient:
+    """
+    Клиент для работы с API NSI.
+    """
+
+    def __init__(self, request_handler: RequestHandler):
+        """
+        Инициализация NSIClient.
+
+        :param request_handler: Экземпляр RequestHandler для отправки HTTP-запросов.
+        """
+        self.request_handler = request_handler
+
+    def get_count_reference_books(self) -> int:
+        """
+        Возвращает количество доступных справочников в NSI.
+
+        :return: Количество справочников.
+        """
+        response = self.request_handler.get(
+            "https://nsi.rosminzdrav.ru/dictionaries?query="
+        )
+        tree = lxml.html.document_fromstring(response.text)
+        return int(
+            tree.xpath("//div[@class='list-toolbar-title']")[0]
+            .text_content()
+            .split()[2]
+        )
+
+    def get_reference_books(
+        self, size: int = -1
+    ) -> Generator[List[Dict[str, Any]], None, None]:
+        """
+        Генератор справочников из NSI.
+
+        :param size: Количество записей на странице. По умолчанию возвращает все записи.
+        :yields: Список с данными о справочниках.
+        """
+        size = self.get_count_reference_books() if size == -1 else size
+        page = 1
+
+        while True:
+            response = self.request_handler.get(
+                f"https://nsi.rosminzdrav.ru/dictionaries?page={page}&size={size}"
+            )
+            tree = lxml.html.document_fromstring(response.text)
+            reference_books = tree.xpath("//h4[@class='ant-list-item-meta-title']/a")
+            if not reference_books:
+                break
+
+            result = [
+                {
+                    "identifier": reference_book.get("href").split("/")[2],
+                    "meta-title": reference_book.text_content(),
+                    "version": reference_book.get("href").split("/")[4],
+                }
+                for reference_book in reference_books
+            ]
+
+            yield result
+            page += 1
+
+    def get_link_download_reference_book(
+        self, identifier: str, version: str, format: str = "json"
+    ) -> str:
+        """
+        Возвращает URL для скачивания определенного справочника.
+
+        :param identifier: Идентификатор справочника.
+        :param version: Версия справочника.
+        :param format: Формат файла.
+        :return: URL для скачивания.
+        """
+        response = self.request_handler.get(
+            f"https://nsi.rosminzdrav.ru/api/dataFiles?identifier={identifier}&version={version}&format={format.upper()}"
+        )
+        res = response.json()
+        if res:
+            return f"https://nsi.rosminzdrav.ru/api/dataFiles/{res[0]}"
+        else:
+            print(f'nth found - {identifier}, {version}')
+            print(f"https://nsi.rosminzdrav.ru/api/dataFiles?identifier={identifier}&version={version}&format={format.upper()}")
+
+    def extract_and_load_json(self, url: str, format: str = "json") -> dict:
+        """
+        Скачивает ZIP-архив по URL, извлекает из него JSON-файл и загружает данные из файла.
+
+        :param url: URL для скачивания файла.
+        :param format: Формат файла внутри архива (по умолчанию JSON).
+        :return: Данные, извлеченные из файла.
+        """
+        # Определение имен файлов
+        zip_file_name = url.split("/")[-1]  # Имя ZIP-файла
+        json_file_name = zip_file_name.replace(
+            f"_{format}.zip", f".{format}"
+        )  # Преобразуем имя в формат JSON
+
+        # Скачиваем и сохраняем ZIP-архив
+        response = self.request_handler.get(url)
+        with open(zip_file_name, "wb") as zip_file:
+            zip_file.write(response.content)
+
+        # Извлекаем JSON-файл из архива
+        with zipfile.ZipFile(zip_file_name, "r") as zip_ref:
+            zip_ref.extract(json_file_name)
+
+        # Определение кодировки файла для корректного чтения
+        with open(json_file_name, "rb") as file:
+            raw_data = file.read()
+            encoding = chardet.detect(raw_data)["encoding"]
+
+        # Читаем данные из JSON-файла с учетом его кодировки
+        with open(json_file_name, "r", encoding=encoding) as file:
+            data = json.load(file)
+
+        # Удаляем временные файлы
+        os.remove(zip_file_name)
+        os.remove(json_file_name)
+
+        return data
+
+    def get_reference_books_with_rows(
+        self, size: int = -1
+    ) -> Generator[Dict[str, Any], None, None]:
+        """
+        Генератор справочников с данными о строках для каждого из них.
+
+        :param size: Количество записей на странице.
+        :yields: Справочник с данными о строках.
+        """
+        for reference_books in self.get_reference_books(size):
+            for reference_book in reference_books:
+                url = self.get_link_download_reference_book(
+                    reference_book["identifier"], reference_book["version"]
+                )
+                reference_book["rows"] = self.extract_and_load_json(url)["records"]
+            yield reference_books
+
+
+if __name__ == "__main__":
+    request_handler = RequestHandler(verify_ssl=False)
+    nsi_client = NSIClient(request_handler)
+    # g = nsi_client.get_reference_books_with_rows(size=2)
+    # res = next(g)
+    # print(res)
+    g = nsi_client.get_link_download_reference_book(identifier='1.2.643.5.1.13.13.11.1038', version='16.1')
+    res = nsi_client.extract_and_load_json(g)
+    pprint(res)
---
+++ nsirefbooks.py
@@ -0,0 +1,213 @@
+import io
+import os
+import re
+import time
+import xml.etree.ElementTree as ET
+from collections import Counter
+from pprint import pprint
+import lxml
+from lxml import etree
+from nsi_parsing import *
+
+
+class RefBook:
+
+    def __init__(self, kwargs, tag, namespace, path=None, etalon=False):
+        self.tag = tag
+        self.namespace = namespace
+        self.code = kwargs.get('code')
+        self.codeSystem = kwargs.get('codeSystem')
+        self.codeSystemVersion = kwargs.get('codeSystemVersion')
+        self.codeSystemName = kwargs.get('codeSystemName')
+        self.displayName = kwargs.get('displayName')
+        self.etalon = etalon
+        self.path = path
+
+    def __repr__(self):
+        return str(self.__dict__) + '\n'
+
+    def print_attribs(self, other=None):
+        print(self.__dict__.get('path'))
+        if other is not None:
+            correct_res = 'Одинаковые атрибуты: ' + '\n'
+            wrong_res = 'Неодинаковые атрибуты: ' + '\n'
+            for attr_name in self.__dict__:
+                if attr_name not in ('etalon', 'path', 'tag', 'namespace'):
+                    if self.__dict__.get(attr_name) == other.__dict__.get(attr_name):
+                        correct_res += (f'{attr_name} - {"в эталоне" if self.etalon else "в формируемом"}: {self.__dict__.get(attr_name)}, '
+                                f'{"в эталоне" if other.etalon else "в формируемом"}: {other.__dict__.get(attr_name)}') + '\n'
+                    else:
+                        wrong_res += (f'{attr_name} - {"в эталоне" if self.etalon else "в формируемом"}: {self.__dict__.get(attr_name)}, '
+                                f'{"в эталоне" if other.etalon else "в формируемом"}: {other.__dict__.get(attr_name)}') + '\n'
+            print(correct_res, wrong_res, sep='\n')
+        else:
+            res = 'Атрибуты' + (' в формируемом:' if not self.etalon else ' в эталоне') + '\n'
+            for attr_name in self.__dict__:
+                if attr_name not in ('etalon', 'path', 'tag', 'namespace'):
+                    res += f'{attr_name} - {self.__dict__.get(attr_name)}' + '\n'
+            print(res)
+
+    def __eq__(self, other):
+        if isinstance(other, RefBook):
+            if (self.codeSystem == other.codeSystem and
+                    self.codeSystemName == other.codeSystemName and
+                    self.codeSystemVersion == other.codeSystemVersion):
+                return True
+            elif self.codeSystem == other.codeSystem:
+                return True
+        return NotImplemented
+
+
+def get_xml_doc(path: str):
+    example_path = os.path.join(path)
+    with io.open(example_path, "rb") as f:
+        document_example = f.read()
+
+    return document_example
+
+
+def get_elements_by_name(parent_element, elem_namespace, elem_name: str, ns: dict, elem_cls, parents, etalon=False):
+
+    elems = parent_element.xpath(f'//{elem_namespace}:' + f'{elem_name}', namespaces=ns)
+    elems_attrib = []
+    for i, elem in enumerate(elems):
+        elem.attrib['path'] = find_ancestors(elem, parents)[1]
+        elems_attrib += [elem_cls(elem.attrib, etalon)]
+        if elem.attrib.get('codeSystem') is None:
+            print(f'{i} {elem_name} is None')
+            ancestors = find_ancestors(elem, parents)
+
+    return elems_attrib
+
+
+def get_elements_by_attribute(xml_tree, attr_name: str):
+    elements = []
+    for elem in xml_tree.iter():
+        if attr_name in elem.attrib:
+            elements += [elem]
+    return elements
+
+
+def get_only_elem_name(element):
+    if isinstance(element, lxml.etree._Comment):
+        return element.text
+    if isinstance(element, lxml.etree._Element):
+        return str(re.sub('{.*}', '', str(element.tag)))
+
+
+def get_namespace(element):
+    if isinstance(element, lxml.etree._Element):
+        match = re.search('{(.*?)}', element.tag)
+        if match:
+            if match.group(1) != 'urn:hl7-org:v3':
+                return match.group(1)
+            else:
+                return 'ns'
+
+
+def find_ancestors(element, parents: dict):
+    ancestors = []
+    path = [get_only_elem_name(element)]
+    element_set = (id(element), element)
+    while element_set is not None:
+        element_set = parents.get(element_set)
+        if element_set is not None:
+            element = element_set[1]
+            ancestors += [element]
+            path[-1] += f'[{element_set[2]}]'
+            path += [get_only_elem_name(element)]
+            element_set = (element_set[0], element_set[1])
+
+    return ancestors, '/'.join(reversed(path))
+
+
+def get_parent_map(tree):
+    parent_map = {}
+    for parent in tree.iter():
+        cnt_number_of_elements = {get_only_elem_name(child): 1 for child in parent}
+
+        for child_number, child in enumerate(child for child in parent):
+            parent_map[(id(child), child)] = (id(parent), parent, cnt_number_of_elements[get_only_elem_name(child)])
+            cnt_number_of_elements[get_only_elem_name(child)] += 1
+
+    return parent_map
+
+
+def compare_ref_books_names(elements, key_code=None, key_name=None):
+
+    cnt_right = 0
+    i = 0
+
+    request_handler = RequestHandler(verify_ssl=False)
+    nsi_client = NSIClient(request_handler)
+
+    for el in elements:
+        i += 1
+        print('-' * 100 + str(i) + '-' * 100)
+
+        link = nsi_client.get_link_download_reference_book(identifier=el.codeSystem, version=el.codeSystemVersion)
+        if not link:
+            print('-' * 202 + '\n\n\n')
+            continue
+        res = nsi_client.extract_and_load_json(link)
+
+        found = 0
+        found_matching = 0
+        ans = []
+
+        for record in res.get('records'):
+            if (el.code in map(str, record.values()) or
+                    (el.code in map(str, record.get('data').values()) if record.get('data') else 0)):
+                found = 1
+                if (el.displayName.strip() in record.values() or
+                        (el.displayName in map(str, record.get('data').values()) if record.get('data') else 0)):
+                    cnt_right += 1
+                    # print(f'совпадают имена: \n {el.displayName}')
+                    # print(record)
+                    found_matching = 1
+                else:
+                    ans_record = "\n".join(f'{key}: {val}' for key, val in record.items())
+                    ans_el = "\n".join(f'{key}: {val}' for key, val in el.__dict__.items())
+                    ans += [f'Не совпадают имена:\nrecord:\n{ans_record}\n\nelement:\n{ans_el}\n']
+
+        if not found:
+            print(f'Нет record c ключом {el.code}, codeSystem: {el.codeSystem}, version: {el.codeSystemVersion}')
+        elif not found_matching:
+            print(*ans)
+
+        print('-' * 202 + '\n\n\n')
+
+    print(f'Всего элементов с атрибутами (code, codeSystem, codeSystemVersion, displayName) в xml: {len(elements)}, '
+          f'всего правильно указанных атрибутов displayName: {cnt_right}')
+
+
+def get_RefBook_elements_by_attribute(xml_path, attribute):
+
+    xml_data = get_xml_doc(xml_path)
+    tree = etree.fromstring(xml_data)
+    parent_map = get_parent_map(tree)
+
+    RefBook_elements = []
+    OIDs = tuple()
+    lxml_elements = get_elements_by_attribute(tree, attribute)
+
+    for el in lxml_elements:
+        rb_el = RefBook(el.attrib, get_only_elem_name(el), get_namespace(el), find_ancestors(el, parent_map)[1])
+
+        if rb_el.codeSystem and rb_el.code and rb_el.codeSystemVersion and rb_el.displayName:
+            RefBook_elements += [rb_el]
+            OIDs += (rb_el.codeSystem,)
+
+    return RefBook_elements
+
+
+if __name__ == "__main__":
+    t0 = time.perf_counter()
+
+    # задать path
+    elements = get_RefBook_elements_by_attribute(xml_path='/home/vista/MyPycharmProjects/vista_scripts/nsi/test.xml',
+                                                 attribute='codeSystem')
+    compare_ref_books_names(elements=elements)
+
+    t1 = time.perf_counter()
+    print(f'Время работы: {t1 - t0}')
